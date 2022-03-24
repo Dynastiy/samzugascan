45611546cfa9ab041cf696c97e69bd7da716c224
Nsisong Akpan <sandersakpan@gmail.com>
2022-03-24 14:57:08 +0100

    implemented vie all block and txns page

---
+++ src/views/pages/txnsPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="txns__wrap">
+      <h5>All Transactions</h5>
+      <div class="txns__content mt-5">
+        <div class="table-responsive">
+          <table class="table table-centered table-nowrap mb-0">
+            <thead>
+              <tr>
+                <th scope="col">Transaction Hash</th>
+                <th scope="col">Block</th>
+                <th scope="col">Timestamp</th>
+                <th scope="col">From</th>
+                <th scope="col">To</th>
+                <th scope="col">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>187328737asHGS.....</td>
+                <td>2108</td>
+                <td>@24 hours ago</td>
+                <td>0xD2D443.....</td>
+                <td>0xD2D43...</td>
+                <td>@1e-7szc <span>($7.90)</span></td>
+              </tr>
+              <tr>
+                <td>187328737asHGS.....</td>
+                <td>2108</td>
+                <td>@24 hours ago</td>
+                <td>0xD2D443.....</td>
+                <td>0xD2D43...</td>
+                <td>@1e-7szc <span>($7.90)</span></td>
+              </tr>
+              <tr>
+                <td>187328737asHGS.....</td>
+                <td>2108</td>
+                <td>@24 hours ago</td>
+                <td>0xD2D443.....</td>
+                <td>0xD2D43...</td>
+                <td>@1e-7szc <span>($7.90)</span></td>
+              </tr>
+              <tr>
+                <td>187328737asHGS.....</td>
+                <td>2108</td>
+                <td>@24 hours ago</td>
+                <td>0xD2D443.....</td>
+                <td>0xD2D43...</td>
+                <td>@1e-7szc <span>($7.90)</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
