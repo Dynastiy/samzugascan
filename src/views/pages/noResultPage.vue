bb0b8500a848fb0bd44016ebd9578dfdf54afcd4
Nsisong Akpan <sandersakpan@gmail.com>
2022-03-25 14:41:34 +0100

    done

---
+++ src/views/pages/noResultPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="no__results__wrap text-center pt-5">
+      <img src="@/assets/img/sad.png" alt="sad emoji" />
+      <h3>Oooops! No Results Found</h3>
+      <p>
+        We couldn’t find your search results. Please check if you typed a <br />
+        correct block or transaction hash or try a different one.
+      </p>
+      <div @click="goBack()" class="back__wrap d-flex justify-content-center">
+        <ion-icon ios="ios-arrow-back" md="ios-arrow-round-back"></ion-icon>
+        <span>back</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
